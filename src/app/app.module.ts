ee29b74282b96d173532f6a5c68e158bec47617b
Arjun Bhusal <rhythemechristthelion@gmail.com>
2022-11-30 19:59:17 +0545

    adding properly right left top bottol center division

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,20 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { TopComponent } from './top/top.component';
+import { BottomComponent } from './bottom/bottom.component';
+import { RightComponent } from './right/right.component';
+import { LeftComponent } from './left/left.component';
+import { CenterComponent } from './center/center.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    TopComponent,
+    BottomComponent,
+    RightComponent,
+    LeftComponent,
+    CenterComponent
   ],
   imports: [
     BrowserModule,
